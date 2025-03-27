68c80653c7176412787ef3791d2d0aaa7a7d0f3c
harrisonzm <124377148+harrisonzm@users.noreply.github.com>
2025-03-27 03:16:48 -0500

    feature: creation courses est resource

--- backend/client-gateaway/src/app.module.ts
+++ backend/client-gateaway/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [],
+  imports: [CoursesModule],
   controllers: [AppController],
 })
 export class AppModule {}
