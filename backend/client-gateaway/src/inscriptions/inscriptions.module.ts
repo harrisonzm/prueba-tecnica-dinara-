3d2308d686c4355ded8a4e6d584d22801bfb5951
harrisonzm <124377148+harrisonzm@users.noreply.github.com>
2025-03-27 03:23:18 -0500

    feature: convert inscriptions module into a microservice

--- backend/client-gateaway/src/inscriptions/inscriptions.module.ts
+++ backend/client-gateaway/src/inscriptions/inscriptions.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
-import { InscriptionsService } from './inscriptions.service';
 import { InscriptionsController } from './inscriptions.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { envs, INSCRIPTIONS_SERVICE } from 'src/config';
 
 @Module({
   controllers: [InscriptionsController],
-  providers: [InscriptionsService],
+  providers: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: INSCRIPTIONS_SERVICE,
+        transport: Transport.TCP,
+        options: {
+          host: envs.inscriptionsMsHost,
+          port: envs.inscriptionsMsPort,
+        },
+      },
+    ]),
+  ],
 })
 export class InscriptionsModule {}
