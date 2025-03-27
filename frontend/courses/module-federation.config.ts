10f872d0041e76decdd66f09771672f3348a469c
harrisonzm <124377148+harrisonzm@users.noreply.github.com>
2025-03-27 18:20:42 -0500

    feature: exposes components

--- frontend/courses/module-federation.config.ts
+++ frontend/courses/module-federation.config.ts
@@ -1,5 +1,17 @@
 export const mfConfig = {
   name: "courses",
-  exposes: {},
-  shared: ["react", "react-dom"],
+  exposes: {
+    "./CourseCard" : "./src/CourseCard/index.tsx",
+    "./CoursesList" : "./src/CoursesList/index.tsx",
+    "./CreateCourseForm": "./src/CreateCourseForm/index.tsx"
+  },
+  shared: {
+    react: { singleton: true, requiredVersion: "^18.2.0" },
+    "react-dom": { singleton: true, requiredVersion: "^18.2.0" },
+    "@mui/material": { singleton: true, requiredVersion: "^5.13.7" },
+    axios: { singleton: true, requiredVersion: "^1.4.0" },
+    typescript: { singleton: true, requiredVersion: "^5.7.3" },
+    "react-router-dom": { singleton: true, requiredVersion:"^7.4.0"}
+  }
+
 };
