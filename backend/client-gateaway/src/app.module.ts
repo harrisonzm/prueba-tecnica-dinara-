c07e919cee8873551f9c1af13c6ac469f637102e
harrisonzm <124377148+harrisonzm@users.noreply.github.com>
2025-03-27 03:19:29 -0500

    feature: creation inscriptions nest resource

--- backend/client-gateaway/src/app.module.ts
+++ backend/client-gateaway/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { CoursesModule } from './courses/courses.module';
+import { InscriptionsModule } from './inscriptions/inscriptions.module';
 
 @Module({
-  imports: [CoursesModule],
+  imports: [CoursesModule, InscriptionsModule],
   controllers: [AppController],
 })
 export class AppModule {}
