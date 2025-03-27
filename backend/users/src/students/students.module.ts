da9a29eead059d7578b49bd9b54cac70ea22a097
harrisonzm <124377148+harrisonzm@users.noreply.github.com>
2025-03-27 04:14:24 -0500

    students service conection to database and implementation

--- backend/users/src/students/students.module.ts
+++ backend/users/src/students/students.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { StudentsController } from './students.controller';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [StudentsController],
   providers: [StudentsService],
 })
--- backend/users/src/students/students.service.ts
+++ backend/users/src/students/students.service.ts
@@ -1,26 +1,91 @@
-import { Injectable } from '@nestjs/common';
-import { CreateStudentDto } from './dto/create-student.dto';
-import { UpdateStudentDto } from './dto/update-student.dto';
+import { HttpException, Injectable } from '@nestjs/common';
+import { DatabaseService } from 'src/database/database.service';
+import { UpdateUserDto, User } from './entities/student.entity';
+import { Prisma } from '@prisma/client';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class StudentsService {
-  create(createStudentDto: CreateStudentDto) {
-    return 'This action adds a new student';
+  constructor(private readonly databaseService: DatabaseService) {}
+
+  async create(createStudentDto: User): Promise<User> {
+    try {
+      console.log('ENTRO AL SERVICIO ----------------------');
+      console.log(createStudentDto);
+      return await this.databaseService.users.create({
+        data: createStudentDto,
+      });
+    } catch (error) {
+      console.log('NO CREADO ------------------------');
+      if ('code' in Object.keys(error)) {
+        if (error.code === 'P2002') {
+          const field =
+            (error.meta?.target as string[])?.join(', ') || 'desconocido';
+          console.error(`❌ Error de unicidad en: ${field}`);
+          throw new RpcException({
+            statusCode: 400,
+            message: `El campo ${field} ya está en uso.`,
+          });
+        }
+      }
+      throw new RpcException({
+        statusCode: 500,
+        message: 'Error interno en el microservicio.',
+      });
+    }
   }
 
-  findAll() {
-    return `This action returns all students`;
+  async createMany(createStudentDto: User[]): Promise<User[]> {
+    return await this.databaseService.users.createManyAndReturn({
+      data: createStudentDto,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} student`;
+  async findAll(): Promise<User[]> {
+    return await this.databaseService.users.findMany();
   }
 
-  update(id: number, updateStudentDto: UpdateStudentDto) {
-    return `This action updates a #${id} student`;
+  async findOne(id: string): Promise<User> {
+    const user: User | null = await this.databaseService.users.findFirst({
+      where: { id },
+    });
+    if (!user) {
+      throw new RpcException(`No se encontró el estudiante con ID: ${id}`);
+    }
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} student`;
+  async update(
+    id: string,
+    updateStudentDto: UpdateUserDto,
+  ): Promise<User | null> {
+    try {
+      return await this.databaseService.users.update({
+        where: { id },
+        data: updateStudentDto,
+      });
+    } catch (error) {
+      if ('code' in Object.keys(error)) {
+        if (error.code === 'P2025') {
+          throw new RpcException(`No se encontró el estudiante con ID: ${id}`);
+        }
+      } else if (error instanceof Prisma.PrismaClientValidationError) {
+        throw new RpcException('Datos inválidos en la actualización.');
+      }
+      throw new HttpException('El estudiante no existe', 404);
+    }
+  }
+
+  async remove(id: string): Promise<User | null> {
+    try {
+      return await this.databaseService.users.delete({ where: { id } });
+    } catch (error) {
+      if ('code' in Object.keys(error)) {
+        if (error.code === 'P2025') {
+          throw new RpcException(`No se encontró el estudiante con ID: ${id}`);
+        }
+      }
+      throw new HttpException('El estudiante no existe', 404);
+    }
   }
 }
