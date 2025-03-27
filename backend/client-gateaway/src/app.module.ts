0eb21bdc2b3319bfda099e0c33d1e42ce9cc639b
harrisonzm <124377148+harrisonzm@users.noreply.github.com>
2025-03-27 03:27:43 -0500

    feature:create sharedClientsModule to abilitate usage of multiple services in controllers

--- backend/client-gateaway/src/app.module.ts
+++ backend/client-gateaway/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { CoursesModule } from './courses/courses.module';
 import { InscriptionsModule } from './inscriptions/inscriptions.module';
+import { UsersModule } from './users/users.module';
+import { SharedClientsModule } from './utils/sharedClientsModule';
 
 @Module({
-  imports: [CoursesModule, InscriptionsModule],
+  imports: [CoursesModule, InscriptionsModule, UsersModule, SharedClientsModule],
   controllers: [AppController],
 })
 export class AppModule {}
