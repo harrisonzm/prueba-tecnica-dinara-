133de32d3d535c15df0632d56e9cb58bf85adb22
harrisonzm <124377148+harrisonzm@users.noreply.github.com>
2025-03-27 17:42:59 -0500

    feature: connection inscriptions microservice

--- backend/client-gateaway/src/inscriptions/inscriptions.controller.ts
+++ backend/client-gateaway/src/inscriptions/inscriptions.controller.ts
@@ -1,34 +1,110 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { InscriptionsService } from './inscriptions.service';
-import { CreateInscriptionDto } from './dto/create-inscription.dto';
-import { UpdateInscriptionDto } from './dto/update-inscription.dto';
+import {
+  Get,
+  Post,
+  Body,
+  Delete,
+  Query,
+  Inject,
+  HttpException,
+  InternalServerErrorException,
+  Controller,
+} from '@nestjs/common';
+import { Inscription } from './inscriptions.types';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import {
+  INSCRIPTIONS_SERVICE,
+  COURSES_SERVICE,
+  USERS_SERVICE,
+} from 'src/config';
+import { firstValueFrom } from 'rxjs';
+import { Course } from 'src/courses/courses.types';
+import { User } from 'src/users/users.types';
+import { handleRpcError } from 'src/utils';
 
-@Controller('courses/inscriptions')
+@Controller('/courses/inscriptions')
 export class InscriptionsController {
-  constructor(private readonly inscriptionsService: InscriptionsService) {}
+  constructor(
+    @Inject(INSCRIPTIONS_SERVICE)
+    private readonly inscriptionsClient: ClientProxy,
+    @Inject(COURSES_SERVICE) private readonly courseClient: ClientProxy,
+    @Inject(USERS_SERVICE) private readonly studentClient: ClientProxy,
+  ) {}
 
   @Post()
-  create(@Body() createInscriptionDto: CreateInscriptionDto) {
-    return this.inscriptionsService.create(createInscriptionDto);
+  async createInscription(
+    @Body() createInscriptionDto: Inscription,
+  ): Promise<Inscription> {
+    const { idUser, idCourse } = createInscriptionDto;
+    try {
+      // Verificar si el estudiante existe
+      const student: User = await firstValueFrom(
+        this.studentClient.send('findOneStudent', idUser),
+      );
+      if (!student) {
+        throw new HttpException('El estudiante no existe', 404);
+      }
+
+      // Verificar si el curso existe y si hay cupo disponible
+      const course: Course = await firstValueFrom(
+        this.courseClient.send('findOneCourse', idCourse),
+      );
+      if (!course) {
+        throw new HttpException('El curso no existe', 404);
+      }
+
+      const enrolledStudents: Inscription[] = await firstValueFrom(
+        this.inscriptionsClient.send('findAllInscriptions', {
+          idCourse: createInscriptionDto.idCourse,
+        }),
+      );
+
+      if (enrolledStudents.length >= course.limit) {
+        throw new HttpException('No hay cupo en el curso', 400);
+      }
+
+      // Crear la inscripción si no hay errores
+      return await firstValueFrom(
+        this.inscriptionsClient.send('createInscription', createInscriptionDto),
+      );
+    } catch (error) {
+      throw handleRpcError(error, 'Error al inscribir al estudiante');
+    }
   }
 
   @Get()
-  findAll() {
-    return this.inscriptionsService.findAll();
+  async findAllInscriptions(
+    @Query() query: { idUser?: string; idCourse?: string },
+  ): Promise<Inscription | Inscription[]> {
+    try {
+      if (query.idUser || query.idCourse) {
+        return await firstValueFrom<Inscription[]>(
+          this.inscriptionsClient.send('findAllInscriptions', query),
+        );
+      } else {
+        return await firstValueFrom<Inscription>(
+          this.inscriptionsClient.send('findAllInscriptions', query),
+        );
+      }
+    } catch (error) {
+      throw handleRpcError(error, 'error al encontrar las inscripciones');
+    }
   }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.inscriptionsService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateInscriptionDto: UpdateInscriptionDto) {
-    return this.inscriptionsService.update(+id, updateInscriptionDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.inscriptionsService.remove(+id);
+  @Delete()
+  async removeInscription(
+    @Query() query: { idUser?: string; idCourse?: string }
+  ): Promise<Inscription | Inscription[]> {
+    try {
+      if (!query.idUser && !query.idCourse) {
+        return await firstValueFrom<Inscription[]>(
+          this.inscriptionsClient.send('deleteInscription', query),
+        );
+      } else {
+        return await firstValueFrom<Inscription>(
+          this.inscriptionsClient.send('deleteInscription', query),
+        );
+      }
+    } catch (error) {
+      throw handleRpcError(error, 'error al eliminar la inscripción');
+    }
   }
 }
