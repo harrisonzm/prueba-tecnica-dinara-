fe3af9344dc376224d5c1ae6879acd64780febd1
harrisonzm <124377148+harrisonzm@users.noreply.github.com>
2025-03-27 17:44:14 -0500

    feature: users connection with gateaway

--- backend/client-gateaway/src/users/users.controller.ts
+++ backend/client-gateaway/src/users/users.controller.ts
@@ -11,7 +11,8 @@ import {
 import { INSCRIPTIONS_SERVICE, USERS_SERVICE } from '../config/services';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
-import { User, UpdateUser } from './dto/users.types';
+import { User, UpdateUser } from './users.types';
+import { handleRpcError } from 'src/utils';
 @Controller('users/student')
 export class UsersController {
   constructor(
@@ -22,17 +23,32 @@ export class UsersController {
 
   @Post()
   async createStudent(@Body() User: User) {
-    
+    console.log('🛠 Enviando datos:', User);
+    try {
+      return await firstValueFrom<User>(
+        this.userClient.send('createStudent', User),
+      );
+    } catch (error: unknown) {
+      throw handleRpcError(error, 'error al crear un estudiante');
+    }
   }
 
   @Get()
   async findAllStudents(): Promise<User[]> {
-    
+    try {
+      return await firstValueFrom(this.userClient.send('findAllStudents', {}));
+    } catch (error) {
+      throw handleRpcError(error, 'error al buscar los estudiantes');
+    }
   }
 
   @Get(':id')
   async findOneStudent(@Param('id') id: string): Promise<User> {
-    
+    try {
+      return await firstValueFrom(this.userClient.send('findOneStudent', id));
+    } catch (error) {
+      throw handleRpcError(error, 'error al buscar el estudiante');
+    }
   }
 
   @Patch(':id')
@@ -40,10 +56,33 @@ export class UsersController {
     @Param('id') id: string,
     @Body() updateUser: UpdateUser,
   ): Promise<User> {
-    
+    try {
+      console.log('ID en la URL:', id);
+      console.log('Datos recibidos:', updateUser);
+
+      // Sobrescribe el ID en el DTO
+      const updatedData = { ...updateUser, id };
+
+      return await firstValueFrom<User>(
+        this.userClient.send('updateStudent', updatedData),
+      );
+    } catch (error: unknown) {
+      throw handleRpcError(error, 'error al actualizar el usuario');
+    }
   }
 
   @Delete(':id')
   async removeStudent(@Param('id') id: string): Promise<User> {
-   
+    try {
+      const student: User = await firstValueFrom<User>(
+        this.userClient.send('deleteStudent', id),
+      );
+      await firstValueFrom(
+        this.inscriptionsClient.send('deleteInscription', { idUser: id }),
+      );
+      return student;
+    } catch (error) {
+      throw handleRpcError(error, 'error al eliminar el estudiante');
+    }
+  }
 }
