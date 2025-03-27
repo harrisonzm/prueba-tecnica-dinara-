e6da0aab6fb7e5391bd8c47642931235971c7711
harrisonzm <124377148+harrisonzm@users.noreply.github.com>
2025-03-27 03:22:23 -0500

    feature: convert users module into a microservice

--- backend/client-gateaway/src/users/users.module.ts
+++ backend/client-gateaway/src/users/users.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { envs } from 'src/config/envs';
+import { USERS_SERVICE } from 'src/config/services';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [],
+  imports: [
+    ClientsModule.register([
+      {
+        name: USERS_SERVICE,
+        transport: Transport.TCP,
+        options: { host: envs.usersMsHost, port: envs.usersMsPort },
+      },
+    ]),
+  ],
 })
 export class UsersModule {}
